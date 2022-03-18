3fc151067e6d803a0c841beca457af41198a7d1d
Albert Tulo <atuloiv@gmail.com>
2022-03-18 04:50:09 -0500

    initial push

---
+++ MainMethodAssignment/MainMethodAssignment/Calculations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Calculations
+    {
+        public int AddUp(int x)
+        {
+            int inputA = x + 3;
+            return inputA;
+        }
+
+        public int AddUp(decimal y)
+        {
+            decimal inputB = y - 34.5m;
+            return Convert.ToInt32(inputB);
+        }
+        public int AddUp (string z)
+        {
+            int inputC = Convert.ToInt32(z);
+            int inputD = inputC * 4;
+            return inputD;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new object form Calculations class. 
+            //taking user input and stores it to a variable. calls upon MathUp method and calculates accordingly
+            Calculations inputA = new Calculations();
+            Console.WriteLine("Submit a whole number.");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int result1 = inputA.AddUp(a);
+            Console.WriteLine(a + " plus 3 equals {0}.", result1);
+            Console.ReadLine();
+
+            Console.WriteLine("Please submit a decimal number to be subtracted by 34.5");
+            decimal b = Convert.ToDecimal(Console.ReadLine());
+            int result2 = inputA.AddUp(b);
+            Console.WriteLine("this equals {0}.", result2);
+            Console.ReadLine(); 
+
+            Console.WriteLine("Please submit another Whole Number.");
+            string c = Console.ReadLine();
+            int result3 = inputA.AddUp(c);
+            Console.WriteLine(c + " times 4 equals {0}.", result3);
+            Console.ReadLine();
+        }
+    }
+}
