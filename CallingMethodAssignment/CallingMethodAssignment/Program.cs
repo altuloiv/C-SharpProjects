9a31397aa9ae7666ea8ae2774d6bb87d74fe427e
Albert Tulo <atuloiv@gmail.com>
2022-03-18 04:29:39 -0500

    Initial push

---
+++ CallingMethodAssignment/CallingMethodAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CallingMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //asking for anumber and storing it to a variable
+            Console.WriteLine("What number would you like to calculate?\n Please enter a number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adding 30 to " + number);
+
+            //creating new object of Sub class and names it as Object
+            Sub Object = new Sub();
+            //access the sub class method of SubIt
+            int returnX = Object.AddIt(number);
+            Console.WriteLine(returnX);
+
+            //access the sub class method of SubIt
+            Console.WriteLine("Subtracting 4 from " + number);
+            int returnY = Object.SubIt(number);
+            Console.WriteLine(returnY);
+
+            //access the sub class method of MultiplyIt
+            Console.WriteLine("Multiplying 15 by " + number);
+            int returnZ = Object.TimesIt(number);
+            Console.WriteLine(returnZ);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CallingMethodAssignment/CallingMethodAssignment/Sub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CallingMethodAssignment
+{
+	public class Sub
+	{
+		public int AddIt(int number)
+		{
+			int x = 30;
+			return number + x;
+		}
+		public int SubIt(int number)
+        {
+			int y = 4;
+			return number - y;
+        }
+		public int TimesIt(int number)
+        {
+			int z = 15;
+			return number * z;
+        }
+	}
+}
