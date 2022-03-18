933c8cf3cf464dbc44652371a5783c12ef5f59c6
Albert Tulo <atuloiv@gmail.com>
2022-03-18 06:15:56 -0500

    Initial push

---
+++ AbstractClassAssignment/AbstractClassAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            employee.SayName();
+
+            employee.Id = 001;
+
+            Console.WriteLine(employee);
+        }
+    }
+}
