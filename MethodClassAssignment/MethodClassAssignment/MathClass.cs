a3e52ce84be1785cf9b294e4b3441a9381a7bb15
Albert Tulo <atuloiv@gmail.com>
2022-03-18 18:31:13 -0500

    initial push

---
+++ MethodClassAssignment/MethodClassAssignment/MathClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    public class MathClass
+    {
+        public void Addition(int num1, int num2)
+        {
+            int result = num1 + 5;
+            Console.WriteLine(num2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathClass math1 = new MathClass();
+            MathClass math2 = new MathClass();
+            math1.Addition(10, 34);
+            math2.Addition(num1: 22, num2: 41);
+        }
+    }
+}
