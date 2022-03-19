96740b24c8638f91ab3f6c0ad54a1eef18ded8b0
Albert Tulo <atuloiv@gmail.com>
2022-03-18 22:12:57 -0500

    initial push

---
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDateTime = DateTime.Now;
+
+            Console.WriteLine("Current Date and time is " + currentDateTime.ToString());
+
+            Console.WriteLine("Type a number between 1-24 then press enter");
+
+            int hours = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("In " + hours + " hour(s) it will be " + currentDateTime.AddHours(hours).ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
