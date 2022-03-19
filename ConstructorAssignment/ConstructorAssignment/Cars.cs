96a1481d315a59221251ed9ebf4060153546cf3a
Albert Tulo <atuloiv@gmail.com>
2022-03-19 01:14:36 -0500

    initial push

---
+++ ConstructorAssignment/ConstructorAssignment/Cars.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    public class HP
+    {
+        public HP() : this(null, 0)
+        {
+
+        }
+        public HP(string name, int rank)
+        {
+            Name = name;
+            Rank = rank;
+        }
+        public string Name { get; set; }
+        public int Rank { get; set; }
+    }
+
+}
---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string owner = "Jim Carry";
+            Console.WriteLine(owner);
+            var Cars = new Dictionary<string, int>() { ["Lamborghini"] = 1, ["Ferrari"] = 2, ["Porsche"] = 3, ["Lotsu"] = 4 };
+            Console.WriteLine(Cars["Lamborghini"]);
+            ConstructorAssignment.HP hP = new ConstructorAssignment.HP("Aston Marten", 5);
+            Console.WriteLine(hP.Name + " " + hP.Rank);
+            Console.ReadLine();
+        }
+    }
+}
